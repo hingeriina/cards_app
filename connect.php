42127dad12e44fe080af5ab6ebbaec2bc7419ecd
Riina <hingeroostesriina@gmail.com>
2020-03-20 15:37:05 +0200

    added connect.php - file

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+function connect_db() {
+  $servername = "localhost";
+  // EDIT HERE YOUR USERNAME AND PASSWORD
+  $username = "bgg307";
+  $password = "puKUoP63m";
+  $dbname = "bgg307";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname); // Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error); } else {
+}
+  return $conn;
+}
+?>
